ca274d22777b2a509609707074e252e344db5afa
JuiPharm <jui_3065@hotmail.com>
2026-02-24 22:10:41 +0700

    Add files via upload

--- docs/config.js
+++ docs/config.js
@@ -1,7 +1,4 @@
-// ===== CONFIG (edit this) =====
-// Paste your Google Apps Script Web App URL here (the deployed web app URL).
-// Example: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
 window.APP_CONFIG = {
   API_BASE_URL: "https://script.google.com/macros/s/AKfycbzkmCOtIgo3GTpaCAa8YVvRKCTSsClohgGmaAUEaLMhiA5I3KpjgHT4mg-9JnaqQKpY/exec",
-  ENV_LABEL: "PROD",
+  ENV_LABEL: "PROD"
 };
