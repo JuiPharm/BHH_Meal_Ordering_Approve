af0d4509c4533af29422eefb8e366024781e3080
JuiPharm <jui_3065@hotmail.com>
2026-02-24 21:44:56 +0700

    Update API_BASE_URL in config.js

--- docs/config.js
+++ docs/config.js
@@ -2,6 +2,6 @@
 // Paste your Google Apps Script Web App URL here (the deployed web app URL).
 // Example: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec
 window.APP_CONFIG = {
-  API_BASE_URL: "PASTE_YOUR_GAS_WEB_APP_URL_HERE",
+  API_BASE_URL: "https://script.google.com/macros/s/AKfycbzkmCOtIgo3GTpaCAa8YVvRKCTSsClohgGmaAUEaLMhiA5I3KpjgHT4mg-9JnaqQKpY/exec",
   ENV_LABEL: "PROD",
 };
